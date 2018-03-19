57333bfe46d5de5c6bbc7c805dfc98fcc838d335
Elliot Paull <elliot.paull@bluefruit.co.uk>
2018-03-19 15:14:33 +0000

    Filled git ignore. Set up Allegro.

---
+++ Snake/Snake/Snake.cpp
@@ -0,0 +1,12 @@
+// Snake.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+
+int main()
+{
+	
+}
